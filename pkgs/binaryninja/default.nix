e0d8cda669e634075b24dc9a5a7214453441baf0
th3m1ghtyduck <th3m1ghtyduck@users.noreply.github.com>
2026-09-02 14:20:18 +0100

    chore: track binaryninja

---
+++ pkgs/binaryninja/default.nix
@@ -0,0 +1,40 @@
+{ pkgs, useWayland ? true, ... }:
+let
+  srcData = builtins.fromJSON (builtins.readFile ./source.json);
+  binjaZip = pkgs.requireFile {
+    name = "binaryninja_linux_${srcData.version}_personal.zip";
+    url = "https://binary.ninja";
+    sha256 = srcData.hash;
+  };
+  basePkg = if useWayland then pkgs.binary-ninja-personal-wayland else pkgs.binary-ninja-personal;
+in
+basePkg.overrideAttrs (old: {
+  src = binjaZip;
+
+  nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.python312 pkgs.python312Packages.pycryptodome pkgs.makeWrapper ];
+
+  autoPatchelfIgnoreMissingDeps = (old.autoPatchelfIgnoreMissingDeps or []) ++ [
+    "libQt6WaylandEglClientHwIntegration.so.6"
+  ];
+
+  postInstall = (old.postInstall or "") + ''
+    if [ -d "$out/opt/binaryninja" ]; then
+      cp ${./patch.py} "$out/opt/binaryninja/script.py"
+      cd "$out/opt/binaryninja"
+      python3 ./script.py
+    fi
+  '';
+
+  postFixup = (old.postFixup or "") + ''
+    if [ -f "$out/bin/binaryninja" ]; then
+      wrapProgram "$out/bin/binaryninja" \
+        --set-default PYTHON ${pkgs.python312}/bin/python3 \
+        --prefix PATH : ${pkgs.python312}/bin \
+        --prefix PYTHONPATH : "\$BINJA_PYTHONPATH"
+    fi
+
+    if [ -f "$out/share/applications/Binary Ninja.desktop" ]; then
+      sed -i 's|^Icon=.*|Icon='"$out"'/share/pixmaps/binaryninja.png|' "$out/share/applications/Binary Ninja.desktop"
+    fi
+  '';
+})
